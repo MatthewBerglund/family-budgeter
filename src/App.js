d68a80c23aa8ee331a1b3f9bc3833f7455b7e0b1
Matthew Berglund <bergitastrophy@gmail.com>
2022-03-30 17:44:18 +0200

    Improve HTML semantics

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Expenses from './views/Expenses';
 
 function App() {
   return (
-    <div className="container-md">
+    <main className="container-md">
       <header className="row text-center my-5">
         <h1 className="display-1 text-primary">Mat's Budget</h1>
       </header>
       <Summary />
       <Expenses />
-    </div>
+    </main>
   );
 }
 
