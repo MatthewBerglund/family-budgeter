60762798682092d45caa0df68b513a410fff7388
Matthew Berglund <bergitastrophy@gmail.com>
2022-04-01 16:22:55 +0200

    Add functionality to fetch expenses on initial render

--- src/views/Expenses.js
+++ src/views/Expenses.js
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import AddExpensesForm from '../components/AddExpensesForm';
 import ExpensesList from '../components/ExpensesList';
 
 const Expenses = () => {
   const [expenses, setExpenses] = useState([]);
+
+  // Run on initial render only (dependency list is empty)
+  useEffect(() => {
+    // Function definition pulled inside `useEffect` in order to keep dependency list empty
+    const fetchExpenses = async () => {
+      const token = process.env.REACT_APP_MOSTASH_API_KEY;
+      const url = `http://pi.motine.de:12305/items.json?stash=${token}&kind=expense`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setExpenses(data);
+    };
+
+    fetchExpenses().catch(error => {
+      console.log(error);
+    });
+  }, []);
 
   const addExpense = (title, date, amount) => {
     setExpenses([...expenses, { id: uuid(), title, date, amount }]);
