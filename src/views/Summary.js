822f0e5dd61de2d005e822a222d04acc8aae6a9c
Matthew Berglund <bergitastrophy@gmail.com>
2022-04-06 09:13:19 +0200

    Remove superfluous div

--- src/views/Summary.js
+++ src/views/Summary.js
@@ -14,21 +14,19 @@ const Summary = () => {
       <div className="text-center">
         <h2 className="display-2">Summary</h2>
       </div>
-      <div className="">
-        <h3 className="fw-light">
-          Your monthly budget is
-          <span className="text-primary">{` ${totalBudget}€`}</span>
-        </h3>
-        <h3 className="fw-light">
-          You have spent
-          <span className="text-danger">{` ${totalExpenses}€`}</span>
-        </h3>
-        <h3 className="fw-light">
-          This leaves you with
-          <span className="text-success">{` ${totalSavings}€`}</span> until
-          <span>{' 31.03.22'}</span>
-        </h3>
-      </div>
+      <h3 className="fw-light">
+        Your monthly budget is
+        <span className="text-primary">{` ${totalBudget}€`}</span>
+      </h3>
+      <h3 className="fw-light">
+        You have spent
+        <span className="text-danger">{` ${totalExpenses}€`}</span>
+      </h3>
+      <h3 className="fw-light">
+        This leaves you with
+        <span className="text-success">{` ${totalSavings}€`}</span> until
+        <span>{' 31.03.22'}</span>
+      </h3>
     </section>
   );
 };
