a3e5d763067f2af5b7972244fc4a7516668ba384
Matthew Berglund <bergitastrophy@gmail.com>
2022-04-08 15:06:51 +0200

    Modify helper functions

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,14 +1,12 @@
 // Takes a string representing an amount and returns a new
 // string representing the amount in euros for speakers of UK English
 export function getUKFormattedEuros(stringAmount) {
-  const formattedAmount = Intl
-    .NumberFormat('en-UK', { style: 'currency', currency: 'EUR' })
-    .format(stringAmount);
-  return formattedAmount;
+  const options = { style: 'currency', currency: 'EUR' };
+  return Intl.NumberFormat('en-GB', options).format(stringAmount);
 }
 
 // Takes a date string in `YYYY-MM-DD` format and returns a new string
-// in `DD MONTH YYYY` format
+// in `DD/MM/YYYY` format
 export function getUKFormattedDate(dateString) {
   const dateObj = new Date(dateString);
   return Intl.DateTimeFormat('en-GB').format(dateObj);
