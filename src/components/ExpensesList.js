86591fcaffd5b1da7ba9b7b8f3c3ef2683887abb
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-04-10 07:40:22 +0200

    removed unnecessary condition based on expenses

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -1,6 +1,6 @@
 import Expense from './Expense';
 
-const ExpensesList = ({ expenses, removeExpense, filteredExpenses }) => {
+const ExpensesList = ({ removeExpense, filteredExpenses }) => {
   const renderExpenses = () => {
     return filteredExpenses.map((expense) => (
       <Expense key={expense.id} {...expense} removeExpense={removeExpense} />
@@ -13,7 +13,7 @@ const ExpensesList = ({ expenses, removeExpense, filteredExpenses }) => {
         <h2>History</h2>
       </div>
       <ul className="list-group">
-        {expenses.length > 0 && filteredExpenses.length > 0 ? (
+        {filteredExpenses.length > 0 ? (
           renderExpenses()
         ) : (
           <p>You have no prior expenses</p>
