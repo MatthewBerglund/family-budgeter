ff039fa7c254b17e024b3af44413b1d1e41fc513
Matthew Berglund <bergitastrophy@gmail.com>
2022-04-27 07:40:15 +0200

    Add logic to calculate spending rate deviation

--- src/views/Summary.js
+++ src/views/Summary.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import { getUKFormattedEuros } from '../utils/helpers';
 
-function Summary({ selectedMonth, filteredExpenses }) {
+function Summary({ currentMonth, selectedMonth, filteredExpenses }) {
   const [totalBudget, setTotalBudget] = useState(7777 * 100);
   const [totalExpenses, setTotalExpenses] = useState(0);
+  const [spendingRateDeviation, setSpendingRateDeviation] = useState(0);
 
+  // Calculate total expenses
   useEffect(() => {
-    // Calculate total expenses
     const expenseSum = filteredExpenses
       .map(expense => expense.amount)
       .reduce((sum, curr) => {
@@ -14,6 +15,27 @@ function Summary({ selectedMonth, filteredExpenses }) {
       }, 0);
     setTotalExpenses(expenseSum);
   }, [filteredExpenses]);
+
+  // Calculate spending rate deviation
+  useEffect(() => {
+    const date = new Date(selectedMonth);
+
+    // Get last day of selected month
+    const daysInMonth = new Date(
+      date.getFullYear(),
+      date.getMonth() + 1,
+      0
+    ).getDate();
+
+    // Set day equal to today's date or last day of the month
+    const dayOfMonth =
+      selectedMonth === currentMonth ? new Date().getDate() : daysInMonth;
+
+    const targetRate = totalBudget / daysInMonth;
+    const actualRate = totalExpenses / dayOfMonth;
+    const deviation = (actualRate - targetRate) / targetRate;
+    setSpendingRateDeviation(deviation);
+  }, [currentMonth, selectedMonth, totalBudget, totalExpenses]);
 
   return (
     <div className="card h-100">
@@ -41,9 +63,15 @@ function Summary({ selectedMonth, filteredExpenses }) {
           <span className="col-6 col-md-4 col-lg-6 col-xl-5">
             Remaining budget:
           </span>
-          <span className="col-4 text-warning fw-bold text-end">{` ${getUKFormattedEuros(
-            totalBudget - totalExpenses
-          )}`}</span>
+          <span
+            className={`col-4 fw-bold text-end ${
+              spendingRateDeviation >= 0.15
+                ? 'text-danger'
+                : spendingRateDeviation > 0
+                ? 'text-warning'
+                : 'text-success'
+            }`}
+          >{` ${getUKFormattedEuros(totalBudget - totalExpenses)}`}</span>
           <div className="col-3"></div>
         </h4>
       </div>
