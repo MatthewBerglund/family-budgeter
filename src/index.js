6dd9b341fd24a96c8ea7251ba9f596328ef20427
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-05-02 09:40:49 +0200

    chore(bootstrap): add js bundle and sass to complete bootstrap installation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+
+import './index.scss';
 
 const root = document.getElementById('root');
 render(<App />, root);
