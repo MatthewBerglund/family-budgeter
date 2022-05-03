f5827fd6a4fd63f737ac808b1010e9ea32e2eaee
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-05-03 22:31:27 +0200

    feat(expense): add confirmation before deletion

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,30 +1,37 @@
 import { getUKFormattedDate, getUKFormattedEuros } from '../utils/helpers';
+import ConfirmModal from './ConfirmModal';
 
-const Expense = ({ title, amount, date, id, removeExpense }) => {
+const Expense = ({ expense, removeExpense }) => {
+  const { title, amount, date, id } = expense;
+
   return (
-    <li className="container gy-5 px-0 list-group-item">
-      <div className="row align-items-center">
-        <div className="col-9 col-lg-10">
-          <div className="row align-items-center">
-            <div className="col-lg-2 fs-5 fw-bold text-nowrap">
-              {getUKFormattedDate(date)}
-            </div>
-            <div className="col-lg-8 fs-5">{title}</div>
-            <div className="col-lg-2 fs-5 text-lg-end text-nowrap">
-              - {getUKFormattedEuros(amount)}
+    <>
+      <li className="container gy-5 px-0 list-group-item">
+        <div className="row align-items-center">
+          <div className="col-9 col-lg-10">
+            <div className="row align-items-center">
+              <div className="col-lg-2 fs-5 fw-bold text-nowrap">
+                {getUKFormattedDate(date)}
+              </div>
+              <div className="col-lg-8 fs-5">{title}</div>
+              <div className="col-lg-2 fs-5 text-lg-end text-nowrap">
+                - {getUKFormattedEuros(amount)}
+              </div>
             </div>
           </div>
+          <div className="col-3 col-lg-2 text-end">
+            <button
+              className="btn btn-danger"
+              data-bs-toggle="modal"
+              data-bs-target={`#modal${id}`}
+            >
+              Delete
+            </button>
+          </div>
         </div>
-        <div className="col-3 col-lg-2 text-end">
-          <button
-            className="btn btn-danger"
-            onClick={() => removeExpense({ title, amount, date, id })}
-          >
-            Delete
-          </button>
-        </div>
-      </div>
-    </li>
+      </li>
+      <ConfirmModal removeExpense={removeExpense} expense={expense} />
+    </>
   );
 };
 
--- src/views/ExpenseHistory.js
+++ src/views/ExpenseHistory.js
@@ -6,24 +6,26 @@ const ExpensesList = ({ removeExpense, filteredExpenses }) => {
   });
 
   return (
-    <div className="card">
-      <h3 className="card-header text-start">History</h3>
-      <div className="card-body">
-        <ul className="list-group list-group-flush" data-cy="expenses">
-          {filteredExpenses.length > 0 ? (
-            sortedExpenses.map(expense => (
-              <Expense
-                key={expense.id}
-                {...expense}
-                removeExpense={removeExpense}
-              />
-            ))
-          ) : (
-            <p className="m-0">You have no prior expenses</p>
-          )}
-        </ul>
+    <>
+      <div className="card">
+        <h3 className="card-header text-start">History</h3>
+        <div className="card-body">
+          <ul className="list-group list-group-flush" data-cy="expenses">
+            {filteredExpenses.length > 0 ? (
+              sortedExpenses.map(expense => (
+                <Expense
+                  key={expense.id}
+                  expense={expense}
+                  removeExpense={removeExpense}
+                />
+              ))
+            ) : (
+              <p className="m-0">You have no prior expenses</p>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
