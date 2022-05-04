3df6f71a73d5bcf618f5f24055191cc0b50f0ba7
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-05-04 10:37:43 +0200

    feat(addExpenses): add modal to confirm month to show

--- src/views/AddExpenses.js
+++ src/views/AddExpenses.js
@@ -1,14 +1,21 @@
 import { useState } from 'react';
-import { convertEurosToCents } from '../utils/helpers';
+import { convertEurosToCents, getUKFormattedDate } from '../utils/helpers';
 
-const AddExpenses = ({ addExpense }) => {
+import ConfirmMonthModal from '../components/ConfirmMonthModal';
+
+const AddExpenses = ({ addExpense, selectedMonth, setSelectedMonth }) => {
   const [title, setTitle] = useState('');
   const [date, setDate] = useState('');
   const [amount, setAmount] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [monthToShow, setMonthToShow] = useState(selectedMonth);
 
   const handleSubmit = evt => {
     evt.preventDefault();
     const expense = { title, date, amount: convertEurosToCents(amount) };
+    if (monthToShow !== selectedMonth) {
+      setIsModalOpen(true);
+    }
     addExpense(expense);
     setTitle('');
     setDate('');
@@ -16,61 +23,78 @@ const AddExpenses = ({ addExpense }) => {
   };
 
   return (
-    <div className="card h-100">
-      <h3 className="card-header">Add expense</h3>
-      <div className="card-body">
-        <form className="row g-2" onSubmit={handleSubmit}>
-          <div className="col-lg-6">
-            <div className="form-floating">
+    <>
+      <div className="card h-100">
+        <h3 className="card-header">Add expense</h3>
+        <div className="card-body">
+          <form className="row g-2" onSubmit={handleSubmit}>
+            <div className="col-lg-6">
+              <div className="form-floating">
+                <input
+                  className="form-control"
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  onChange={evt => setTitle(evt.target.value)}
+                  value={title}
+                  placeholder="Expense Name"
+                />
+                <label htmlFor="name">Expense name</label>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="form-floating">
+                <input
+                  className="form-control"
+                  type="date"
+                  id="date"
+                  name="date"
+                  required
+                  onChange={evt => {
+                    setDate(evt.target.value);
+                    setMonthToShow(
+                      getUKFormattedDate(evt.target.value, {
+                        year: 'numeric',
+                        month: 'long',
+                      })
+                    );
+                  }}
+                  value={date}
+                  placeholder="Expense Date"
+                />
+                <label htmlFor="date">Expense date</label>
+              </div>
+            </div>
+            <div className="form-floating mb-2">
               <input
                 className="form-control"
-                type="text"
-                id="name"
-                name="name"
+                type="number"
+                id="amount"
+                name="amount"
                 required
-                onChange={evt => setTitle(evt.target.value)}
-                value={title}
-                placeholder="Expense Name"
+                min="0.01"
+                step="0.01"
+                onChange={evt => setAmount(evt.target.value)}
+                value={amount}
+                placeholder="Euro amount (with comma and two decimal places)"
               />
-              <label htmlFor="name">Expense name</label>
+              <label htmlFor="amount">Expense amount in €</label>
             </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="form-floating">
-              <input
-                className="form-control"
-                type="date"
-                id="date"
-                name="date"
-                required
-                onChange={evt => setDate(evt.target.value)}
-                value={date}
-                placeholder="Expense Date"
-              />
-              <label htmlFor="date">Expense date</label>
-            </div>
-          </div>
-          <div className="form-floating mb-2">
-            <input
-              className="form-control"
-              type="number"
-              id="amount"
-              name="amount"
-              required
-              min="0.01"
-              step="0.01"
-              onChange={evt => setAmount(evt.target.value)}
-              value={amount}
-              placeholder="Euro amount (with comma and two decimal places)"
-            />
-            <label htmlFor="amount">Expense amount in €</label>
-          </div>
-          <button className="btn btn-primary" type="submit">
-            Add
-          </button>
-        </form>
+            <button className="btn btn-primary" type="submit">
+              Add
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+      {isModalOpen && (
+        <ConfirmMonthModal
+          setIsModalOpen={setIsModalOpen}
+          setSelectedMonth={setSelectedMonth}
+          monthToShow={monthToShow}
+        />
+      )}
+    </>
   );
 };
 
