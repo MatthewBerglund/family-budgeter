c847eb47a03d0ce39c94a8c8e6126cd81b292104
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-05-09 11:36:46 +0200

    chore(modals): restructure modals instances

--- src/components/Modals/ConfirmDeleteModal.js
+++ src/components/Modals/ConfirmDeleteModal.js
@@ -1,68 +1,31 @@
-import { useRef, useEffect } from 'react';
-import {
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-} from './components/ModalComponents';
+import { Modal } from './components/Modal';
 import { getUKFormattedDate, getUKFormattedEuros } from '../../utils/helpers';
 
-const ConfirmDeleteModal = ({ expense, setIsOpen, handleCallForAction }) => {
-  const callForAction = useRef(null);
-
-  // focus on call for action button on mount
-  // blur on unmount
-  useEffect(() => {
-    callForAction.current?.focus();
-
-    // the ref must be saved into a variable to accomodate react warning
-    const elementToClean = callForAction.current;
-    return () => {
-      elementToClean.blur();
-    };
-  }, []);
-
+const ConfirmDeleteModal = ({ expense, setIsOpen, okCallback }) => {
   const { title, amount, date, id } = expense;
 
+  const modalProps = {
+    cancelCallback: () => setIsOpen(false),
+    okCallback: () => okCallback(id),
+    modalTitle: 'Confirm expense deletion',
+    cancelButtonLabel: "I'm not sure",
+    okButtonLabel: 'Yes delete!!',
+    okButtonColor: 'btn-danger',
+  };
+
   return (
-    <Modal modalId={`modal${id}`}>
-      <ModalHeader setIsOpen={setIsOpen}>
-        <h5 className="modal-title" id="modalLabel">
-          Confirm expense deletion
-        </h5>
-      </ModalHeader>
-      <ModalBody>
-        <h6 className="mb-3">
-          Please confirm you want to delete this expense:
-        </h6>
-        {expense && (
-          <dl className="row">
-            <dt className="col-3">Date:</dt>
-            <dd className="col-9">{getUKFormattedDate(date)}</dd>
-            <dt className="col-3">Title:</dt>
-            <dd className="col-9">{title}</dd>
-            <dt className="col-3">Amount:</dt>
-            <dd className="col-9">{getUKFormattedEuros(amount)}</dd>
-          </dl>
-        )}
-      </ModalBody>
-      <ModalFooter>
-        <button
-          type="button"
-          className="btn btn-secondary"
-          onClick={() => setIsOpen(false)}
-        >
-          I'm not sure
-        </button>
-        <button
-          type="button"
-          className="btn btn-danger"
-          ref={callForAction}
-          onClick={() => handleCallForAction(id)}
-        >
-          Yes delete!!
-        </button>
-      </ModalFooter>
+    <Modal {...modalProps}>
+      <h6 className="mb-3">Please confirm you want to delete this expense:</h6>
+      {expense && (
+        <dl className="row">
+          <dt className="col-3">Date:</dt>
+          <dd className="col-9">{getUKFormattedDate(date)}</dd>
+          <dt className="col-3">Title:</dt>
+          <dd className="col-9">{title}</dd>
+          <dt className="col-3">Amount:</dt>
+          <dd className="col-9">{getUKFormattedEuros(amount)}</dd>
+        </dl>
+      )}
     </Modal>
   );
 };
--- src/components/Modals/ConfirmMonthModal.js
+++ src/components/Modals/ConfirmMonthModal.js
@@ -1,55 +1,20 @@
-import { useRef, useEffect } from 'react';
-import {
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-} from './components/ModalComponents';
+import { Modal } from './components/Modal';
 
-const ConfirmMonthModal = ({ monthToShow, setIsOpen, handleCallForAction }) => {
-  const callForAction = useRef(null);
-
-  // focus on call for action button on mount
-  // blur on unmount
-  useEffect(() => {
-    callForAction.current?.focus();
-
-    // the ref must be saved into a variable to accomodate react warning
-    const elementToClean = callForAction.current;
-    return () => {
-      elementToClean.blur();
-    };
-  }, []);
+const ConfirmMonthModal = ({ monthToShow, setIsOpen, okCallback }) => {
+  const modalProps = {
+    cancelCallback: () => setIsOpen(false),
+    okCallback: () => okCallback(monthToShow),
+    modalTitle: 'Select month view',
+    cancelButtonLabel: 'Stay here',
+    okButtonLabel: `Open ${monthToShow}`,
+    okButtonColor: 'btn-primary',
+  };
 
   return (
-    <Modal modalId="confirmationHistoryView">
-      <ModalHeader setIsOpen={setIsOpen}>
-        <h5 className="modal-title" id="modalLabel">
-          Select month view
-        </h5>
-      </ModalHeader>
-      <ModalBody>
-        <h6 className="mb-3">
-          Would you like to open the new expense month's history?
-        </h6>
-      </ModalBody>
-      <ModalFooter>
-        <button
-          type="button"
-          className="btn btn-secondary"
-          onClick={() => setIsOpen(false)}
-        >
-          Stay here
-        </button>
-        <button
-          type="button"
-          className="btn btn-primary"
-          ref={callForAction}
-          onClick={handleCallForAction}
-        >
-          Open {monthToShow}
-        </button>
-      </ModalFooter>
+    <Modal {...modalProps}>
+      <h6 className="mb-3">
+        Would you like to open the new expense month's history?
+      </h6>
     </Modal>
   );
 };
