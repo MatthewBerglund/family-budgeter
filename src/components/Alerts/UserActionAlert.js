8732b5e66938b07f8f6dc345d2c6db44205a8415
Matthew Berglund <bergitastrophy@gmail.com>
2022-06-05 07:46:11 +0200

    Add documentation

--- src/components/Alerts/UserActionAlert.js
+++ src/components/Alerts/UserActionAlert.js
@@ -1,6 +1,16 @@
 import ExpenseAddedAlert from './ExpenseAddedAlert';
 import ExpenseDeletedAlert from './ExpenseDeletedAlert';
 
+/**
+ * A dynamic alert component that displays feedback information based on a `userAction`.
+ *
+ * @param userAction {string} an action performed by the user
+ * Available actions: 'add_expense' | 'delete_expense'
+ * @param errorOccurred {bool} `true` if an error was caught when the user action was performed, otherwise `false`
+ * @param closeCallback {function} to be called when the close ('X') or 'Cancel' buttons are clicked
+ * @param expense {object} information about the expense that the user action was performed on (title, amount, date, id)
+ */
+
 const UserActionAlert = ({
   userAction,
   errorOccurred,
@@ -11,8 +21,10 @@ const UserActionAlert = ({
 
   switch (userAction) {
     case 'add_expense':
+      // required props: errorOccurred, closeCallback
       return <ExpenseAddedAlert {...baseProps} />;
     case 'delete_expense':
+      // required props: errorOccurred, closeCallback, expense
       return <ExpenseDeletedAlert {...baseProps} {...expense} />;
     default:
       return null;
