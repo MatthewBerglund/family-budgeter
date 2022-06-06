f4d1c29ce470748fa24dfe0e639999d9a88ff935
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-06-07 00:16:37 +0200

    chore(test): add support folder and eslint pref

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: false,
+    supportFile: 'cypress/support/e2e.js',
   },
 });
