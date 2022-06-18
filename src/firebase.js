6b70cd333492bbb72ee644a18001c0bbf8db06c8
Matthew Berglund <bergitastrophy@gmail.com>
2022-06-18 16:55:10 +0200

    Reset firebase config, remove rtdb from emulator config

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,16 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, connectDatabaseEmulator } from 'firebase/database';
+import { getDatabase } from 'firebase/database';
 
-const firebaseConfig = {
+let firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}-default-rtdb.europe-west1.firebasedatabase.app`,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: "704102984911",
-  appId: "1:704102984911:web:915b77d7bac3a83fd55bba"
+  authDomain: 'family-budgeter-1.firebaseapp.com',
+  projectId: 'family-budgeter-1',
+  storageBucket: 'family-budgeter-1.appspot.com',
+  messagingSenderId: '314882244523',
+  appId: '1:314882244523:web:262b12470ec58b441f1cee',
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-
-if (process.env.REACT_APP_EMULATE === true) {
-  connectDatabaseEmulator(db, 'localhost', 9000);
-}
 
 export default db;
