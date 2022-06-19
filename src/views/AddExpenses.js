3d2d1d1b221c39789b79fa4f56ebb6df32fa879d
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-06-19 11:19:55 +0200

    chore(src-views): adapt to new printWidth settings

--- src/views/AddExpenses.js
+++ src/views/AddExpenses.js
@@ -32,9 +32,7 @@ const AddExpenses = ({ addExpense, setNewExpenseMonth }) => {
     setWasDateValidated(true);
     // newExpenseMonth is needed to compare with selected month
     // and trigger the ConfirmMonthModal if they are different
-    setNewExpenseMonth(
-      getUKFormattedDate(date, { year: 'numeric', month: 'long' })
-    );
+    setNewExpenseMonth(getUKFormattedDate(date, { year: 'numeric', month: 'long' }));
   };
 
   const handleAmountChange = evt => {
@@ -47,24 +45,11 @@ const AddExpenses = ({ addExpense, setNewExpenseMonth }) => {
       <h3 className="card-header">Add expense</h3>
       <div className="card-body">
         <form className="row g-2" onSubmit={handleSubmit}>
-          <div
-            className={`col-lg-6 ${wasTitleValidated ? 'was-validated' : ''}`}
-          >
+          <div className={`col-lg-6 ${wasTitleValidated ? 'was-validated' : ''}`}>
             <label className="form-label" htmlFor="name">
               Expense name
             </label>
-            <input
-              className="form-control"
-              type="text"
-              id="name"
-              name="name"
-              placeholder="e.g. Groceries"
-              aria-describedby="nameHelp"
-              required
-              autoFocus
-              value={title}
-              onChange={handleTitleChange}
-            />
+            <input className="form-control" type="text" id="name" name="name" placeholder="e.g. Groceries" aria-describedby="nameHelp" required autoFocus value={title} onChange={handleTitleChange} />
           </div>
           <div
             className={`col-lg-6 ${wasDateValidated ? 'was-validated' : ''}`}
@@ -76,40 +61,15 @@ const AddExpenses = ({ addExpense, setNewExpenseMonth }) => {
             <label className="form-label" htmlFor="date">
               Expense date
             </label>
-            <DatePicker
-              className="form-control"
-              id="date"
-              name="date"
-              placeholderText="DD/MM/YYYY"
-              aria-describedby="dateHelp"
-              required
-              selected={date}
-              onChange={handleDateChange}
-              dateFormat="dd/MM/yyyy"
-              todayButton="Today"
-            />
+            <DatePicker className="form-control" id="date" name="date" placeholderText="DD/MM/YYYY" aria-describedby="dateHelp" required selected={date} onChange={handleDateChange} dateFormat="dd/MM/yyyy" todayButton="Today" />
           </div>
-          <div
-            className={`col mb-2 ${wasAmountValidated ? 'was-validated' : ''}`}
-          >
+          <div className={`col mb-2 ${wasAmountValidated ? 'was-validated' : ''}`}>
             <label className="form-label" htmlFor="amount">
               Expense amount
             </label>
             <div className="input-group">
               <span className="input-group-text">€</span>
-              <input
-                className="form-control"
-                type="number"
-                id="amount"
-                name="amount"
-                placeholder="e.g. 19.99"
-                aria-describedby="amountHelp"
-                required
-                min="0.01"
-                step="0.01"
-                value={amount}
-                onChange={handleAmountChange}
-              />
+              <input className="form-control" type="number" id="amount" name="amount" placeholder="e.g. 19.99" aria-describedby="amountHelp" required min="0.01" step="0.01" value={amount} onChange={handleAmountChange} />
             </div>
           </div>
           <button className="btn btn-primary" type="submit">
