e54cf58b9947bc82f4567a849eb94c2e2466e9af
GiulianoMontisEvernest <giuliano.montis@evernest.com>
2022-06-19 11:20:06 +0200

    chore(src-views): adapt to new printWidth settings

--- src/views/ExpenseHistory.js
+++ src/views/ExpenseHistory.js
@@ -1,12 +1,6 @@
 import Expense from '../components/Expense';
 
-const ExpensesHistory = ({
-  filteredExpenses,
-  setConfirmDeleteModalIsOpen,
-  setEditExpenseModalIsOpen,
-  setLastDeleted,
-  setExpenseToEdit,
-}) => {
+const ExpensesHistory = ({ filteredExpenses, setConfirmDeleteModalIsOpen, setEditExpenseModalIsOpen, setLastDeleted, setExpenseToEdit }) => {
   const sortedExpenses = filteredExpenses.sort((expenseA, expenseB) => {
     return new Date(expenseB.date) - new Date(expenseA.date);
   });
@@ -16,20 +10,7 @@ const ExpensesHistory = ({
       <h3 className="card-header text-start">History</h3>
       <div className="card-body">
         <ul className="list-group list-group-flush" data-cy="expenses">
-          {filteredExpenses.length > 0 ? (
-            sortedExpenses.map(expense => (
-              <Expense
-                key={expense.id}
-                expense={expense}
-                setConfirmDeleteModalIsOpen={setConfirmDeleteModalIsOpen}
-                setEditExpenseModalIsOpen={setEditExpenseModalIsOpen}
-                setLastDeleted={setLastDeleted}
-                setExpenseToEdit={setExpenseToEdit}
-              />
-            ))
-          ) : (
-            <p className="m-0">You have no prior expenses</p>
-          )}
+          {filteredExpenses.length > 0 ? sortedExpenses.map(expense => <Expense key={expense.id} expense={expense} setConfirmDeleteModalIsOpen={setConfirmDeleteModalIsOpen} setEditExpenseModalIsOpen={setEditExpenseModalIsOpen} setLastDeleted={setLastDeleted} setExpenseToEdit={setExpenseToEdit} />) : <p className="m-0">You have no prior expenses</p>}
         </ul>
       </div>
     </div>
