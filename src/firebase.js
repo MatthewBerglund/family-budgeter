52e361949955079fff8ff8ab76ef634ec30e75a1
Matthew Berglund <bergitastrophy@gmail.com>
2022-06-20 09:56:50 +0200

    Set up firestore emulator

--- src/firebase.js
+++ src/firebase.js
@@ -13,4 +13,8 @@ let firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
+if (window.location.hostname === 'localhost') {
+  db.useEmulator('localhost', 8080);
+}
+
 export default db;
