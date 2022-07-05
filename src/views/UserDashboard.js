d18d57ff9a369df08ff81603d813917169b33b9b
Matthew Berglund <bergitastrophy@gmail.com>
2022-07-05 16:03:48 +0200

    Remove memoization

--- src/views/UserDashboard.js
+++ src/views/UserDashboard.js
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect, useMemo } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 import Summary from './Summary';
 import AddExpenses from './AddExpenses';
@@ -20,17 +20,15 @@ const UserDashboard = () => {
   const [selectedMonth, setSelectedMonth] = useState(currentMonth);
   const [isConfirmMonthModalOpen, setIsConfirmMonthModalOpen] = useState(false);
 
-  const selectedMonthExpenses = useMemo(() => {
-    return expenses
-      .filter(expense => getExpenseMonth(expense) === selectedMonth)
-      .sort((expenseA, expenseB) => new Date(expenseB.date) - new Date(expenseA.date));
-  }, [expenses, selectedMonth]);
+  const selectedMonthExpenses = expenses
+    .filter(expense => getExpenseMonth(expense) === selectedMonth)
+    .sort((expenseA, expenseB) => new Date(expenseB.date) - new Date(expenseA.date));
 
   useEffect(() => {
     if (selectedMonthExpenses.length === 0) {
       changeMonthView(currentMonth);
     }
-  }, [selectedMonthExpenses, currentMonth]);
+  });
 
   const changeMonthView = (month) => {
     setSelectedMonth(month);
